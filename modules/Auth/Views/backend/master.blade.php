ad68ba0eae5fa8227dc435f7cd0af40a10862bad
Phúc HP <phuchp.613@gmail.com>
2021-05-10 16:57:52 +0700

    Update route and view

---
+++ modules/Auth/Views/backend/master.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('assets/fontawesome/css/all.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/select2/css/select2.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/datetimepicker/css/datetimepicker.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/backend/css/main.css') }}">
+    <title>Login Page</title>
+</head>
+<body style="background-image: url('/logo/bg-login.jpg'); background-size: cover; background-repeat: no-repeat">
+    @yield('content')
+</body>
+<script src="{{ asset('assets/jquery/jquery-3.5.1.min.js') }}"></script>
+<script src="{{ asset('assets/bootstrap/datetimepicker/js/datetimepicker.min.js') }}"></script>
+<script src="{{ asset('assets/select2/js/select2.min.js') }}"></script>
+<script src="{{ asset('assets/backend/jquery/main.js') }}"></script>
+<script src="{{ asset('assets/backend/jquery/custom.js') }}"></script>
+</html>
